cf1d3122ab12af05d11e51e3a05e652f88485e6e
João Augusto <jhonaugustjunior@gmail.com>
2020-12-07 23:33:36 -0300

    page 404 configuration

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { Menu } from '../components/Menu'
+import styles from '../styles/theme/theme.module.scss'
+import { GoAlert } from 'react-icons/go'
+import { IoIosArrowBack } from 'react-icons/io'
 
 const Custom404: React.FC = () => {
-  return <h1>404 - Page Not Found</h1>
+  return (
+    <>
+      <Menu />
+      <div className={styles.error404}>
+        <GoAlert className={styles.icon} />
+        <h2>404 - Página Não Encontrada!</h2>
+        <a className={styles.linkSecondary} href="/">
+          <IoIosArrowBack />
+          Voltar para a Home
+        </a>
+      </div>
+    </>
+  )
 }
 
 export default Custom404
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 import '../styles/globals.scss'
+import Head from 'next/head'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>João Augusto</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
